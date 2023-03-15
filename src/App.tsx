d3e63a1d33c8e4f74cc385e7bec14db5d0fa06c7
Rustam Guseinov <rg20031991.com>
2023-03-15 17:10:03 +0300

    build commit with two required  pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import Home from './pages/Home'
+import {  BrowserRouter as Router,  Routes,  Route} from "react-router-dom";
+import Home from './pages/Home';
+import ListUsers from './pages/ListUsers';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
-      <Home/>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/users" element={<ListUsers />} />
+          </Routes>
+        </Router>
     </div>
   );
 }
